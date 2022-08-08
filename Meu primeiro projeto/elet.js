8beb48cde3ca22608703cadd03666a43cd082041
Raimundo Ribeiro <RaimundoRibeiro123@users.noreply.github.com>
2022-08-08 14:28:02 -0300

    Update elet.js

--- Meu primeiro projeto/elet.js
+++ Meu primeiro projeto/elet.js
@@ -1,30 +1,28 @@
 function calcular(){
-    var r1=window.document.getElementById("re1")
-    var r2=window.document.getElementById("re2")
-    var r3=window.document.getElementById("re3")
-    var req=window.document.getElementById("req")
-    var ra =Number.parseInt(r1.value)
-    var rb =Number.parseInt(r2.value)
-    var rc =Number.parseInt(r3.value)
-    var rp1 =(1/ra+1/rb)
-    var res1=1/rp1
-    var rp2=(1/ra+1/rb+1/rc)
-    var res2=1/rp2
-    
-if ((rp1==1/ra+1/rb && res1>=1000)){
-    req.innerHTML= `Valor equivalente: ${res1.toFixed(1)} k&Omega;`
+    let r1=window.document.getElementById("re1")
+    let r2=window.document.getElementById("re2")
+    let r3=window.document.getElementById("re3")
+    let req=window.document.getElementById("req")
+    let ra =Number.parseInt(r1.value)
+    let rb =Number.parseInt(r2.value)
+    let rc =Number.parseInt(r3.value)
+    let rp1 =1/ra+1/rb
+    let veq1=1/rp1
+    let rp2=1/ra+1/rb+1/rc
+    let veq2=1/rp2
+
+if (rp1==(1/ra+1/rb) && (veq1>=1000)){
+    req.innerHTML= `Valor equivalente: ${veq1.toFixed()} k&Omega;`
 }
-else{
-   req.innerHTML= `Valor equivalente: ${res1.toFixed(1)} &Omega;`
+else if (veq1<1000){
+    req.innerHTML= `Valor equivalente: ${veq1.toFixed()} &Omega;` 
+}
+if (rp2 == (1/ra+1/rb+1/rc) && (veq2>=1000)){
+   req.innerHTML= `Valor equivalente: ${veq2.toFixed()} k&Omega;`
+}
+else if(veq2<1000){
+    req.innerHTML= `Valor equivalente: ${veq2.toFixed()} &Omega;`
+}
 }
 
-if  (rp2 == 1/ra+1/rb+1/rc && res2>=1000){
-   req.innerHTML= `Valor equivalente: ${res2.toFixed(1)} k&Omega;`
-}
-else {
-    req.innerHTML= `Valor equivalente: ${res2.toFixed(1)} &Omega;`
-}
-}
 document.write(Date());
-
-
